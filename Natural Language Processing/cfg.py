37462ce35b0812b0a4bf63e4bed9d44a5f5d533d
Samip Lamsal <lamsalsamip@outlook.com>
2023-07-04 21:18:27 +0545

    Context Free Grammer Parsing

---
+++ Natural Language Processing/cfg.py
@@ -0,0 +1,26 @@
+## NLTK -> Natural Language Toolkit
+import nltk
+
+
+#More rules you add, more complex sentences can be parsed
+grammar = nltk.CFG.fromstring("""
+
+S -> NP VP
+
+NP -> D N | N
+VP -> V | V NP
+D -> "the" | "a"
+N -> "she" | "city" | "car"
+V -> "saw" | "walked"
+""")
+
+parser = nltk.ChartParser(grammar)
+
+sentence = input("Sentence: ").split()
+
+try:
+    for tree in parser.parse(sentence):
+        tree.pretty_print()
+        tree.draw()
+except ValueError:
+    print("No parse tree possible")
